7a6859ffc87faf57bf4db5e0b80d87c1b7700748
Andrew Zehrer <andrewzehrer@tamu.edu>
2026-04-30 17:56:19 -0500

    initial steps for prover

--- Project/unify.py
+++ Project/unify.py
@@ -37,6 +37,33 @@ def parse_expr(s):
     return parse(tokenize(s))
 
 
+# read and parse a knowledge base file
+def parse_kb_file(filename):
+    with open(filename, "r") as f:
+        text = f.read()
+
+    tokens = tokenize(text)
+    expressions = []
+
+    # parse multiple top-level expressions
+    while tokens:
+        expressions.append(parse(tokens))
+
+    kb = []
+
+    for expr in expressions:
+        # rule: (implies body head)
+        if isinstance(expr, list) and len(expr) == 3 and expr[0] == "implies":
+            body = expr[1]
+            head = expr[2]
+            kb.append(("rule", body, head))
+        else:
+            # treat everything else as a fact
+            kb.append(("fact", expr))
+
+    return kb
+
+
 # ----------------------------
 # utilities
 # ----------------------------
@@ -124,6 +151,45 @@ def unify_var(var, x, subst):
 
 
 # ----------------------------
+# proving
+# ----------------------------
+
+def prove(goal, kb, subst):
+    results = []
+
+    for item in kb:
+        if item[0] == "fact":
+            fact = item[1]
+            new_subst = unify(goal, fact, subst.copy())
+            if new_subst is not None:
+                results.append(new_subst)
+
+        elif item[0] == "rule":
+            body, head = item[1], item[2]
+
+            new_subst = unify(goal, head, subst.copy())
+            if new_subst is not None:
+                results.extend(prove_body(body, kb, new_subst))
+
+    return results
+
+
+def prove_body(body, kb, subst):
+    # handle (and ...)
+    if isinstance(body, list) and body[0] == "and":
+        results = [subst]
+        for subgoal in body[1:]:
+            new_results = []
+            for s in results:
+                sub_results = prove(subgoal, kb, s)
+                new_results.extend(sub_results)
+            results = new_results
+        return results
+    else:
+        return prove(body, kb, subst)
+    
+
+# ----------------------------
 # printing
 # ----------------------------
 
@@ -135,33 +201,45 @@ def to_string(x):
         return x
 
 
+def print_kb(kb):
+    print("facts:")
+    for item in kb:
+        if item[0] == "fact":
+            print(" ", to_string(item[1]))
+
+    print("\nrules:")
+    for item in kb:
+        if item[0] == "rule":
+            body, head = item[1], item[2]
+            print(" ", f"{to_string(head)} :- {to_string(body)}") # prolog-style output
+
+
+def print_results(results):
+    if not results:
+        print("no solutions")
+        return
+
+    for res in results:
+        print("solution:")
+        for var in res:
+            val = to_string(substitute(res[var], res))
+            print(f"  {var} = {val}")
+
+
 # ----------------------------
 # main
 # ----------------------------
 
 def main():
-    if len(sys.argv) != 3:
-        print("usage: python unify.py \"expr1\" \"expr2\"")
-        return
+    kb = parse_kb_file("knowledge.txt")
+    print_kb(kb)
+    print()
 
-    expr1 = parse_expr(sys.argv[1])
-    expr2 = parse_expr(sys.argv[2])
+    query = parse_expr("(wins ?X blastoise)")
+    print("query:", to_string(query), "\n")
 
-    subst = unify(expr1, expr2, {})
-
-    if subst is None:
-        print("failure")
-        return
-
-    # print substitutions
-    for var in subst:
-        val = substitute(subst[var], subst)
-        print(f"{var} -> {to_string(val)}")
-
-    # print unified expressions
-    e1 = to_string(substitute(expr1, subst))
-    e2 = to_string(substitute(expr2, subst))
-    print(f"{e1} = {e2}")
+    results = prove(query, kb, {})
+    print_results(results)
 
 
 if __name__ == "__main__":
